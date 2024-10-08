8e592dc19961aaecb5e967a779bb59f929283dce
ROhit-Kumar-Guin <rohitguin006@gmail.com>
2024-10-08 08:41:32 +0530

    modified after cloning

--- gitDemo.java
+++ gitDemo.java
@@ -2,4 +2,10 @@ class Launch {
 	public static void main(Strings[] args){
 		System.out.print("Exploring Git");
 	}
+	public static void fun(){
+		int a=1;
+		int b=2;
+		System.out.print(a+b);
+	}
+
 }
